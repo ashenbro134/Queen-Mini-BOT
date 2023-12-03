bb33d48af8432e8f4cb1b0dbb766171002792d95
ashenbro134 <129660680+ashenbro134@users.noreply.github.com>
2023-12-03 11:33:25 +0530

    💝Queen-Alexa💝

---
+++ plugins/gc-setname.js
@@ -0,0 +1,17 @@
+
+let handler  = async (m, { conn, args, text }) => {
+if (!text) throw `*💝ENTER THE NAME YOU WANT TO BE THE NEW GROUP NAME💝*`
+try {
+let text = args.join` `
+if(!args || !args[0]) {
+} else {
+conn.groupUpdateSubject(m.chat, text)}
+} catch (e) {
+throw '*💝SORRY THERE WAS AN ERROR, THE NAME CAN NOT BE MORE THAN 25 CHARACTERS💝*'
+}}
+handler.help = ['setname <text>']
+handler.tags = ['group']
+handler.command = /^(setname)$/i
+handler.group = true
+handler.admin = true
+export default handler
---
+++ plugins/gp-tagall.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(`💥 Group : *${groupMetadata.subject}*\n💥 Members : *${participants.length}*${text ? `\n💥 Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──💝 𝗔𝗟𝗘𝗫𝗔 𝗕𝗢𝗧 💝──', null, {
+        mentions: users
+    })
+}
+
+handler.help = ['tagall']
+handler.tags = ['group']
+handler.command = ['tagall']
+handler.admin = true
+handler.group = true
+
+export default handler
